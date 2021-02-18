59873890467aff0b31e517c3632aada22ca81dd0
Juliano Blank <julianoellwanger@gmail.com>
2021-02-18 11:18:53 -0300

    Implementado o retorno correto do Json e Tratamento de excecao

--- src/main/java/com/julianoblank/sistemapedido/services/CategoriaService.java
+++ src/main/java/com/julianoblank/sistemapedido/services/CategoriaService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import com.julianoblank.sistemapedido.domain.Categoria;
 import com.julianoblank.sistemapedido.repositories.CategoriaRepository;
 
+import javassist.tools.rmi.ObjectNotFoundException;
+
 @Service
 public class CategoriaService {
 	
@@ -16,7 +18,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(()-> new com.julianoblank.sistemapedido.services.exceptions.ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
 
 }
